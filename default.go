a74903b32bec1ba806e88e6b602e68b9b67436f8
Pablo Lalloni <plalloni@gmail.com>
2019-04-05 13:21:24 -0300

    Introduce métodos de validación directa usando el default validator

--- default.go
+++ default.go
@@ -7,7 +7,15 @@ var Default = MustNew()
 func MustNew() Validator {
 	v, err := New()
 	if err != nil {
-		panic(fmt.Sprintf("Error creating new validator: %v", err))
+		panic(fmt.Sprintf("Error creating default validator: %v", err))
 	}
 	return v
+}
+
+func ValidatePersonaJSON(bs []byte) (*ValidationResult, error) {
+	return Default.ValidatePersonaJSON(bs)
+}
+
+func ValidatePersonaListJSON(bs []byte) (*ValidationResult, error) {
+	return Default.ValidatePersonaListJSON(bs)
 }
